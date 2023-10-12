14bed87638a656fb7b1885db3612fc3218a7ce0b
Ananya Kaushal <62956565+Ananya-Kaushal@users.noreply.github.com>
2023-10-13 00:22:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 //your JS code here. If required.
-// function enlargeIt() {
-	// const para=document.getElementById("status");
-// 	para.innerText="Entered Metaverse"
-// 	para.style.fontSize="31.25px";
-// }
+function enlargeIt() {
+	const para=document.getElementById("status");
+	para.innerText="Entered Metaverse"
+	para.style.fontSize="31.25px";
+}
 
-const btn=document.getElementById("enterBtn");
-const para=document.getElementById("status");
+// const btn=document.getElementById("enterBtn");
+// const para=document.getElementById("status");
 
-btn.addEventListener("click" ,() => {
-	para.innerText="Entered Metaverse";
-})
+// btn.addEventListener("click" ,() => {
+// 	para.innerText="Entered Metaverse";
+// })
